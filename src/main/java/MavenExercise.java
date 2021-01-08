c8612064986f313a8a18a509ca8aa88894530c16
Anthony V <avidaurri4@gmail.com>
2021-01-08 11:07:23 -0600

    solution to maven exercise 1/8

--- src/main/java/MavenExercise.java
+++ src/main/java/MavenExercise.java
@@ -8,7 +8,16 @@ public class MavenExercise {
         Scanner sc = new Scanner(System.in);
         String userInput = sc.nextLine();
 
+        System.out.printf("Your entry: %s\n", userInput);
+        if (StringUtils.isBlank(userInput))
+            System.out.println("Please enter ANYTHING");
+        else if (StringUtils.isNumeric(userInput))
+            System.out.printf("%s is a number!\n", userInput);
+        else
+            System.out.printf("%s not a number!\n", userInput);
 
+        System.out.printf("Flipped: %s\n", StringUtils.swapCase(userInput));
+        System.out.printf("Reversed: %s\n", StringUtils.reverse(userInput));
 
     }
 }
