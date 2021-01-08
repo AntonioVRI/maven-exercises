54eb3278a1db0e83fe2533093b1e97ebe741d3ab
Anthony V <avidaurri4@gmail.com>
2021-01-08 11:05:33 -0600

    getting maven to work

---
+++ src/main/java/MavenExercise.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import org.apache.commons.lang3.StringUtils;
+
+public class MavenExercise {
+    public static void main(String[] args) {
+
+        System.out.println("Enter something here: ");
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+
+
+
+    }
+}
